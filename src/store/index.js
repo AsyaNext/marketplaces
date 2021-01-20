e896ccec87f4156365b8bebd1de1b628ffe3d26f
AsyaNext <nastushaburd@mail.ru>
2021-01-20 20:22:51 +0300

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// import example from './module-example'
+
+Vue.use(Vuex)
+
+export default function (/* { ssrContext } */) {
+  const Store = new Vuex.Store({
+    modules: {
+      // example
+    },
+    strict: process.env.DEBUGGING
+  })
+
+  return Store
+}
