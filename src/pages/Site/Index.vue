463ea06795de342823cfb7fd84e0058388713516
AsyaNext <nastushaburd@mail.ru>
2021-01-21 15:34:26 +0300

    feat: index page

---
+++ src/pages/Site/Index.vue
@@ -0,0 +1,82 @@
+<template>
+  <q-page class="index-page text-white">
+    <section>
+      <div class="index-page-title__functions font-montserrat__semi-bold text-h3">О сервисе</div>
+      <div class="q-pl-lg index-page-function row">
+        <div class="index-page-function__image relative-position">
+          <div class="img relative-position"></div>
+        </div>
+        <div class="index-page-function__column">
+          <div class="index-page-function__title font-montserrat__semi-bold text-h5">Функция</div>
+          <div class="index-page-function__description font-avenir__regular text-main">Описание функции</div>
+        </div>
+      </div>
+      <div class="q-pr-xl index-page-function row justify-end">
+        <div class="index-page-function__column text-right">
+          <div class="index-page-function__title font-montserrat__semi-bold text-h5">Функция</div>
+          <div class="index-page-function__description font-avenir__regular text-main">Описание функции</div>
+        </div>
+        <div class="index-page-function__image relative-position">
+          <div class="img relative-position"></div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="index-page-title__advantages font-montserrat__semi-bold text-h3">Наши преимущества</div>
+      <div class="index-page-advantages">
+        <div class="index-page-advantages__item relative-position">
+          <div class="index-page-advantages__item-number absolute font-montserrat__bold">1</div>
+          <div class="index-page-advantages__item-description text-justify text-main font-avenir__demi-bold">
+            Наличие интернет ресурса дает возможность развивать бизнес за пределами своего
+            географического положения и получить новых клиентов не только в своем регионе,
+            а если вы того захотите то и за ее пределами.
+          </div>
+        </div>
+        <div class="index-page-advantages__item relative-position">
+          <div class="index-page-advantages__item-number absolute font-montserrat__bold">2</div>
+          <div class="index-page-advantages__item-description text-justify text-main font-avenir__demi-bold">
+            Электронный каталог ваших товаров и услуг в каждом доме,
+            доступен 24 часа в сутки, 7 дней в неделю, 365 дней в году.
+          </div>
+        </div>
+        <div class="index-page-advantages__item relative-position">
+          <div class="index-page-advantages__item-number absolute font-montserrat__bold">3</div>
+          <div class="index-page-advantages__item-description text-justify text-main font-avenir__demi-bold">
+            Наличие интернет ресурса дает возможность развивать бизнес за пределами своего
+            географического положения и получить новых клиентов не только в своем регионе,
+            а если вы того захотите то и за ее пределами.
+          </div>
+        </div>
+        <div class="index-page-advantages__item relative-position">
+          <div class="index-page-advantages__item-number absolute font-montserrat__bold">4</div>
+          <div class="index-page-advantages__item-description text-justify text-main font-avenir__demi-bold">
+            Товар ваших конкурентов может быть хуже или дороже, чем ваш,
+            но более популярный лишь потому, что у них есть интернет ресурс.
+          </div>
+        </div>
+        <div class="index-page-advantages__item relative-position">
+          <div class="index-page-advantages__item-number absolute font-montserrat__bold">5</div>
+          <div class="index-page-advantages__item-description text-justify text-main font-avenir__demi-bold">
+            Наличие интернет ресурса дает возможность развивать бизнес за пределами своего
+            географического положения и получить новых клиентов не только в своем регионе,
+            а если вы того захотите то и за ее пределами.
+          </div>
+        </div>
+        <div class="index-page-advantages__item relative-position">
+          <div class="index-page-advantages__item-number absolute font-montserrat__bold">6</div>
+          <div class="index-page-advantages__item-description text-justify text-main font-avenir__demi-bold">
+            Наличие интернет ресурса дает возможность развивать бизнес за пределами своего
+            географического положения и получить новых клиентов не только в своем регионе,
+            а если вы того захотите то и за ее пределами.
+          </div>
+        </div>
+      </div>
+    </section>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
