d71f03a8b2aca81fc66b04a6ff55c2230f790b52
AsyaNext <nastushaburd@mail.ru>
2021-01-23 22:13:00 +0300

    feat: create api.vue

---
+++ src/pages/site/Api.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page></q-page>
+</template>
+
+<script>
+export default {
+  name: 'Api'
+}
+</script>
+
+<style scoped lang="scss">
+  @import "../../css/styles/site/api";
+</style>
