70dceb14bdba60fab97c8c989b51e8b11f9de3f5
AsyaNext <nastushaburd@mail.ru>
2021-01-25 21:53:03 +0300

    fix: build

--- quasar.conf.js
+++ quasar.conf.js
@@ -59,7 +59,6 @@ module.exports = function (/* ctx */) {
 
       // Options below are automatically set depending on the env, set them if you want to override
       // extractCSS: false,
-
       // https://quasar.dev/quasar-cli/handling-webpack
       extendWebpack (cfg) {
         cfg.module.rules.push({
