a04b5fe8f99d97b928cbc92b27cb1cac775f63ce
AsyaNext <nastushaburd@mail.ru>
2021-01-28 16:20:18 +0300

    feat: view Contacts

--- src/pages/site/Contacts.vue
+++ src/pages/site/Contacts.vue
@@ -1,7 +1,35 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="text-h2 font-montserrat__semi-bold text-purple-10">
+  <q-page class="q-mt-lg contacts column">
+    <div class="contacts-headline font-montserrat__semi-bold text-purple-10">
       Контакты
+    </div>
+    <div class="contacts-content column flex-center text-center">
+      <div class="contacts-content__section">
+        <div class="q-mb-md contacts-content__section-title font-montserrat__semi-bold text-h5 text-purple-12">
+          Политика общения
+        </div>
+        <div class="contacts-content__section-text font-avenir__regular text-body1 text-grey-10">
+          Текст, в котором указана информация о политике общения в компании со своими клиентами
+        </div>
+      </div>
+      <div class="contacts-content__section">
+        <div class="q-mb-md contacts-content__section-title font-montserrat__semi-bold text-h5 text-purple-12">
+          Контактные данные
+        </div>
+        <div class="contacts-content__section-list row q-gutter-x-md">
+          <q-icon class="cursor-pointer" size="xl" name="img:icons/icon-telegram.svg" />
+          <q-icon class="cursor-pointer" size="xl" name="img:icons/icon-whatsapp.svg" />
+          <q-icon class="cursor-pointer" size="xl" name="img:icons/icon-viber.svg" />
+          <q-icon class="cursor-pointer" size="xl" name="img:icons/icon-facebook.svg" />
+          <q-icon class="cursor-pointer" size="xl" name="img:icons/icon-vk.svg" />
+        </div>
+      </div>
+      <div class="contacts-content__section">
+        <div class="q-mb-md contacts-content__section-title font-montserrat__semi-bold text-h5 text-purple-12">
+          E-mail
+        </div>
+        <a class="contacts-content__section-email font-avenir__regular text-grey-10" href="mailto:support@gmail.com">support@gmail.com</a>
+      </div>
     </div>
   </q-page>
 </template>
