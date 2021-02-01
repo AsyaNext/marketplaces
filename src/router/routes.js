0bd9798604d4b05b39d42c5dfe719351ca3bd751
AsyaNext <nastushaburd@mail.ru>
2021-02-01 16:12:25 +0300

    fix: route for activate page

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', name: 'index', meta: { title: 'Главная' }, component: () => import('pages/site/Index.vue') },
-      { path: ':uid/:token', name: 'activate', meta: { title: 'Активация' }, component: () => import('pages/Activate.vue') },
+      { path: 'activate/:uid/:token', name: 'activate', meta: { title: 'Активация' }, component: () => import('pages/Activate.vue') },
       { path: 'password/:uid/:token', name: 'password', meta: { title: 'Восстановление пароля' }, component: () => import('pages/Password.vue') },
       {
         path: 'blog',
